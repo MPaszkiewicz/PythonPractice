59bfbe5712d29e5c8bb2bce7e9b4e5b5a9b86bd9
Maksymilian Paszkiewicz <maksymilianpaszkiewicz@gmail.com>
2022-06-25 19:34:54 +0200

    2_BooksDatabase: Add storing data in file

--- 2_BooksDatabase/utils/database.py
+++ 2_BooksDatabase/utils/database.py
@@ -1,16 +1,38 @@
+import json
 
-books = []
+
+database_file = 'database.txt'
 
 
 class DataNotFoundError(Exception):
     pass
 
 
+def read_database():
+    books = []
+    try:
+        with open(database_file, 'r') as database:
+            books = json.load(database)
+    except FileNotFoundError:
+        print("Database file does not exist, returning empty dict.")
+    except json.JSONDecodeError:
+        print("Database file format was incorrect, returning empty dict.")
+    return books
+
+
+def write_database(books):
+    with open(database_file, 'w') as database:
+        json.dump(books, database)
+
+
 def add(name, author, read):
+    books = read_database()
     books.append({'name': name, 'author': author, 'read': read})
+    write_database(books)
 
 
 def get(name):
+    books = read_database()
     for book in books:
         if book['name'] == name:
             return book
@@ -18,14 +40,18 @@ def get(name):
 
 
 def remove(name):
-    global books
+    books = read_database()
     books = [book for book in books if book['name'] != name]
+    write_database(books)
 
 
 def get_all():
-    return books
+    return read_database()
 
 
 def update_read(name, new_read):
-    book = get(name)
-    book['read'] = new_read
+    books = read_database()
+    for book in books:
+        if book['name'] == name:
+            book['read'] = new_read
+    write_database(books)
