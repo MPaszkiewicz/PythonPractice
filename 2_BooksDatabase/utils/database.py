3f7ee968c16dafd0a21181b773f44dd6114a08b6
Maksymilian Paszkiewicz <maksymilianpaszkiewicz@gmail.com>
2022-06-26 19:34:37 +0200

    2_BooksDatabase: Add usage of context manager

--- 2_BooksDatabase/utils/database.py
+++ 2_BooksDatabase/utils/database.py
@@ -11,54 +11,44 @@ BOOKS_TABLE_NAME = 'books'
 
 
 def create_book_table():
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    cursor.execute('CREATE TABLE IF NOT EXISTS {}(name text primary key, author text, read integer)'
-                   .format(BOOKS_TABLE_NAME))
-    connection.commit()
-    connection.close()
+    with DatabaseConnection(DATABASE_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS {}(name text primary key, author text, read integer)'
+                       .format(BOOKS_TABLE_NAME))
 
 
 def add(name, author):
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    cursor.execute('INSERT INTO {} VALUES(?, ?, 0)'.format(BOOKS_TABLE_NAME), (name, author))
-    connection.commit()
-    connection.close()
+    with DatabaseConnection(DATABASE_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO {} VALUES(?, ?, 0)'.format(BOOKS_TABLE_NAME), (name, author))
 
 
 def get(name):
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM {} WHERE name=?'.format(BOOKS_TABLE_NAME), (name,))
-    book = cursor.fetchone()
-    connection.close()
-    if book is None:
-        raise DataNotFoundError("Could not find book with name {}".format(name))
-    return book
+    with DatabaseConnection(DATABASE_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM {} WHERE name=?'.format(BOOKS_TABLE_NAME), (name,))
+        book = cursor.fetchone()
+        if book is None:
+            raise DataNotFoundError("Could not find book with name {}".format(name))
+        return book
 
 
 def remove(name):
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    cursor.execute('DELETE FROM {} WHERE name=?'.format(BOOKS_TABLE_NAME), (name,))
-    connection.commit()
-    connection.close()
+    with DatabaseConnection(DATABASE_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM {} WHERE name=?'.format(BOOKS_TABLE_NAME), (name,))
 
 
 def get_all():
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM {}'.format(BOOKS_TABLE_NAME))
-    books = [{'name': row[0], 'author': row[1], 'read': row[2]}
-             for row in cursor.fetchall()]  # fetchall returns list of tuples
-    connection.close()
-    return books
+    with DatabaseConnection(DATABASE_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM {}'.format(BOOKS_TABLE_NAME))
+        books = [{'name': row[0], 'author': row[1], 'read': row[2]}
+                 for row in cursor.fetchall()]  # fetchall returns list of tuples
+        return books
 
 
 def update_read(name, new_read):
-    connection = sqlite3.connect(DATABASE_FILE)
-    cursor = connection.cursor()
-    cursor.execute('UPDATE {} SET read=? WHERE name=?'.format(BOOKS_TABLE_NAME), (new_read, name))
-    connection.commit()
-    connection.close()
+    with DatabaseConnection(DATABASE_FILE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('UPDATE {} SET read=? WHERE name=?'.format(BOOKS_TABLE_NAME), (new_read, name))
--- 2_BooksDatabase/utils/database_connection.py
+++ 2_BooksDatabase/utils/database_connection.py
@@ -8,6 +8,7 @@ class DatabaseConnection:
 
     def __enter__(self):
         self.connection = sqlite3.connect(self.database)
+        return self.connection
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type or exc_val or exc_tb:
